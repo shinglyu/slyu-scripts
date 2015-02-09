0c195e6fe2551eed8e59ba4f48ff8d9e5c05682c
Shing Lyu <shing.lyu@gmail.com>
2015-02-09 12:14:27 +0800

    Added loggin to test servo

--- test_servo.sh
+++ test_servo.sh
@@ -1,7 +1,9 @@
-./mach build 
-./mach test-tidy
-./mach test-content
-./mach test-wpt-failure 
-./mach test-wpt --process 4
-./mach test-ref
-./mach test-unit
+LOGFILE=/tmp/servo$(date +"%Y%m%d-%H%M").log
+
+./mach build -j 4
+./mach test-tidy             2>&1 |tee -a $LOGFILE
+./mach test-content          2>&1 |tee -a $LOGFILE
+./mach test-wpt-failure      2>&1 |tee -a $LOGFILE
+./mach test-wpt --process 4  2>&1 |tee -a $LOGFILE
+./mach test-ref              2>&1 |tee -a $LOGFILE
+./mach test-unit             2>&1 |tee -a $LOGFILE
