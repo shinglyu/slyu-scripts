650a661bcb473ed055c4ca18b3b1eb5970cb7350
Shing Lyu <shing.lyu@gmail.com>
2015-08-02 12:53:21 +0800

    external dp is the primary window

--- restartDP1.sh
+++ restartDP1.sh
@@ -2,5 +2,5 @@
 xrandr --output eDP1 --auto
 #sleep 1s
 #xrandr --output VGA1 --auto --right-of LVDS1
-xrandr --output DP1 --auto --left-of eDP1 #--rotate left
+xrandr --output DP1 --auto --left-of eDP1 --primary #--rotate left
 i3-msg restart
